0220c8f74d79ee098a9a4b0c0c5153a035dcec6c
archcra@qq.com <archcra@qq.com>
2014-12-05 18:50:22 +0800

    Cause: limbo#105 Fix: rhinoView Verification: Check the result file: rhinoview.png(If images provided, can use a browser to generate the result file.

---
+++ js/scenario-RhinoView.js
@@ -0,0 +1,77 @@
+var scenario = {
+
+	screen : {
+
+		width : 800,
+		height : 5630,
+		SYS_INFO_FONT: "16px 宋体",
+		PROFILE_ID_FONT: "16px 宋体",
+		DIALOG_TEXT_FONT: "20px 宋体",
+		SAYING_FONT: "28px 宋体",
+        GROUP_NAME_X_OFFSET: 100,
+        GROUP_NAME_FONT: "28px 宋体",
+		FONT_RATIO: 1.2
+	},
+	common:{
+		name: "Scene-101",
+		groupName: "Limbo (299792458)" ,
+		withHeader: true,
+
+		saying: "你须相信",
+
+
+
+	},
+	roles:{
+		theme :"waterDrop",
+		waterDrop: "水滴",
+		Athena: "ZHI子",
+		Benboba: "奔波灞",
+		love: "♩♫♬",
+		Baboben: "灞波奔",
+        chanshi:"唐生",
+        letsgomars:"让我们上火星吧",
+        HarukiMurakami:"Haruki Murakami"
+	},
+	dialogs : [
+
+		{SYSTEM: "Fri Dec 05 2014 22:52:00 GMT+0800 (WST)"},
+        {SYSTEM: "凌波微布道(Gossip of Limbo) #105"},
+//{theme: "这仅仅是一个测试，用来测试多行。This is only a test, test for multi-lines. Multi-lines, multi-lines, \njust only multi-lines. Is there multi-lines now?"},
+{letsgomars:"犀牛眼中的世界总有一座山" },
+{SYSTEM_IMAGE : "scenario/rhinoview/rhino_painting" },
+{Athena: "Poor Rhinoceros - He just never figured it out…"},
+{Benboba: "不是吧！明明是粉色的，你PS的吧"},
+{SYSTEM_IMAGE : "scenario/rhinoview/rhino2" },        
+{chanshi: "因为相信　所以看见"},
+{SYSTEM_IMAGE : "scenario/rhinoview/cat_fish_half" }, 
+{love: "I'm like everyone else, I see the world in terms of what I would like to see happen,\nnot what actually does. - The alchemist (Paulo Coelho)"},
+{letsgomars: "距离的远近，是一种感觉"},
+{Baboben: "你的意思，是说我可以在地球上，却感觉和火星非常近？"},
+{chanshi: "夏虫不可语于冰"},
+{love: "Most problems are people problems – recognise that everyone has a different\nperspective　-　such as EBKAC"},  
+{letsgomars: "我们都似盲人摸象，每个人都认为我们的世界是我们自己感知的那个样子"},
+{love: "If the human brain was simple enough for us to understand, we would still be\nso stupid that we couldn't understand it."},
+{Benboba: "在说啥？"},
+{chanshi: "难道我们不能做点什么么？"},
+{letsgomars: "向哲人学习"},     
+{SYSTEM_IMAGE : "scenario/rhinoview/philosophers-sm"},        
+{love: "But if it is an apple tree it will not have the ability to bear pears or plums. The \nsame applies to us humans. We can be hindered in our developmentand our \npersonal growth by political conditions, for instance. Outer circumstances can \nconstrain us. Only when we are free to develop our innate abilities can we live \nas free beings. "},
+{love: "Open your heart"}, 
+{SYSTEM_IMAGE : "scenario/rhinoview/miranda.metheny0e_140117_0433_18"},
+{love: "你看到了什么？"},
+{SYSTEM_IMAGE : "scenario/rhinoview/water_candle_half" },          
+{SYSTEM_IMAGE : "scenario/rhinoview/life_middle" },          
+{HarukiMurakami:"Death is not the opposite of life but an innate part of it."},
+{love: " “I...I’ve watched people. They look at you but they don’t see you, I think. You do\nsomething to their minds.” Death shook his head. They do it all themselves, \nhe said. There’s no magic. People can’t see me, they simply won’t allow\nthemselves to do it. Until it’s time, of course."},
+{love: "You just have to believe"},        
+{SYSTEM_IMAGE : "scenario/rhinoview/jobs_quarter" }
+
+]
+
+
+
+}
+
+
+
